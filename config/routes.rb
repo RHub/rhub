ad84b6480bf9ac41f54c92eafd49aae2f957b70b
Dan Vera <dan@yovu.co>
2012-10-13 16:09:49 -0400

    user dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rhub::Application.routes.draw do
 
   resources :papers
 
-  devise_for :users
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
 
   root :to => 'welcome#home'
 
